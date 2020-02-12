9639a939b4ee699b6836bd449ab552eba18efb19
Billy Ferrara <willfira717@gmail.com>
2020-02-12 19:00:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,3 +55,12 @@ attr_accessor :name, :artist, :genre
 end 
 
 end
+
+
+the_day = Song.new("The Day", "John Smith", "classic")
+tomorow = Song.new("Tomorow", "John Smith", "classic")
+a_yearz = Song.new("A Yearz", "Adam Johns", "oldtime")
+a_rabess = Song.new("Arabess", "Habibi Watan", "Rai")
+cus_umak = Song.new("Cus Ummach", "Haifa Wehbe", "Rai")
+
+
